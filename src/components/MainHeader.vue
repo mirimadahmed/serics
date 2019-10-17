a4735a3e23006a93840dc8e5775cb2003f67b018
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-10-17 19:07:05 +0500

    main header component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>we make</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainHeader"
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
